33558f99ea835fc9b57b072a84688bd6bcd465b5
pratyushvatsalshukla <pratyushpro9333@gmail.com>
2025-12-19 22:26:06 +0530

    Enabled Batch Processing and Component Scan

--- SpringBatch/src/main/java/com/springBatch/SpringBatch/SpringBatchApplication.java
+++ SpringBatch/src/main/java/com/springBatch/SpringBatch/SpringBatchApplication.java
@@ -1,9 +1,13 @@
 package com.springBatch.SpringBatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableBatchProcessing
+@ComponentScan("com.springBatch.config")
 public class SpringBatchApplication {
 
 	public static void main(String[] args) {
