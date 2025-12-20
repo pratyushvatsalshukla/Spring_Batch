79bc22579afa2ae2b088a99a9b911b7a584b25be
pratyushvatsalshukla <pratyushpro9333@gmail.com>
2025-12-20 13:06:50 +0530

    Added Tasklet

--- SpringBatch/src/main/java/com/springBatch/config/service/SecondTasklet.java
+++ SpringBatch/src/main/java/com/springBatch/config/service/SecondTasklet.java
@@ -1,5 +1,6 @@
 package com.springBatch.config.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -7,10 +8,13 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class SecondTasklet implements Tasklet {
     @Override
     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
         System.out.println("This is the 2nd TASKLET Step");
+        log.info("Chunk Job Execution Context : {}",chunkContext.getStepContext().getJobExecutionContext());
+        log.info("Chunk Step Execution Context : {}",chunkContext.getStepContext().getStepExecutionContext().toString());
         return RepeatStatus.FINISHED;
     }
 };
