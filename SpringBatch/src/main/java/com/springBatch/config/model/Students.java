91458b001aa4d02ce2b3cb0f5642d4f1ae61cf5c
pratyushvatsalshukla <pratyushpro9333@gmail.com>
2025-12-22 15:26:04 +0530

    Created Student Schema

---
+++ SpringBatch/src/main/java/com/springBatch/config/model/Students.java
@@ -0,0 +1,18 @@
+package com.springBatch.config.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Students {
+
+    private Long id ;
+    private String firstName ;
+    private String lastName ;
+    private String email ;
+}
