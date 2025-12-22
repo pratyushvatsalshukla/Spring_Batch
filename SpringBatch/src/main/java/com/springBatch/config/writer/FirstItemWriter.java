a636f14029d093d1ef623cb1e0ba32e781b85ae0
pratyushvatsalshukla <pratyushpro9333@gmail.com>
2025-12-22 15:27:11 +0530

    Code for Object printing (WRITER)

--- SpringBatch/src/main/java/com/springBatch/config/writer/FirstItemWriter.java
+++ SpringBatch/src/main/java/com/springBatch/config/writer/FirstItemWriter.java
@@ -1,5 +1,8 @@
 package com.springBatch.config.writer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springBatch.config.model.Students;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.stereotype.Component;
@@ -9,12 +12,20 @@ import java.util.List;
 
 @Component
 @Slf4j
-public class FirstItemWriter  implements ItemWriter<Long> {
+public class FirstItemWriter  implements ItemWriter<Students> {
 
     @Override
-    public void write(List<? extends Long> items) throws Exception {
+    public void write(List<? extends Students> items) throws Exception {
         // ? because Spring Batch writers should accept any subtype produced by processor.
-        log.info("Inside Item Reader ");
-        items.stream().forEach(System.out::println);
+        log.info("Inside Item Writer ");
+        ObjectMapper mapper = new ObjectMapper() ;
+
+        items.stream().forEach(item -> {
+            try {
+                log.info(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(item));
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
