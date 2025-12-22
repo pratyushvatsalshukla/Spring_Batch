6382417bedaf333551f594d8ecbb3fe88cd21ece
pratyushvatsalshukla <pratyushpro9333@gmail.com>
2025-12-22 15:26:13 +0530

    Enabled Async

--- SpringBatch/src/main/java/com/springBatch/SpringBatch/SpringBatchApplication.java
+++ SpringBatch/src/main/java/com/springBatch/SpringBatch/SpringBatchApplication.java
@@ -4,10 +4,12 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableBatchProcessing
-@ComponentScan("com.springBatch.config")
+@EnableAsync
+@ComponentScan({"com.springBatch.config","com.springBatch.controller"})
 public class SpringBatchApplication {
 
 	public static void main(String[] args) {
