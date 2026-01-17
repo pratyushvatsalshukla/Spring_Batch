278b2f27fab88c29edf795cf475304576a88c53f
pratyushvatsalshukla <pratyushpro9333@gmail.com>
2026-01-17 18:20:22 +0530

    Initialized Git Reposotiry

---
+++ springBatch5x/src/main/java/learning/springBatch5x/SpringBatch5xApplication.java
@@ -0,0 +1,13 @@
+package learning.springBatch5x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatch5xApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatch5xApplication.class, args);
+	}
+
+}
---
+++ springBatch5x/src/test/java/learning/springBatch5x/SpringBatch5xApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.springBatch5x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatch5xApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
